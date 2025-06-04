fb23e400b3dd01fb004f89c22867bb458a7913fa
Kyui-azusa-mac <kyuiazusa@gmail.com>
2025-06-04 18:30:41 +0800

    use less

--- routes/notes.js
+++ routes/notes.js
@@ -4,6 +4,18 @@ import { get } from "http";
 const router = express.Router();
 import { marked } from "marked";
 
+marked.setOptions({
+  highlight: function(code, lang) {
+    const language = lang || 'plaintext';
+    return `
+<div class="md-fences md-end-block ty-contain-cm modeLoaded">
+  <pre class="language-${language}"><code>${code}</code></pre>
+</div>
+    `.trim();
+  },
+  langPrefix: 'language-',
+});
+
 router.get("/", (req, res) => {
   res.send("显示所有笔记");
 });
