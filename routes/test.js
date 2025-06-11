2239c735f6044ce1710e1c717849d019a90144f2
Kyui-azusa-mac <kyuiazusa@gmail.com>
2025-06-11 20:11:36 +0800

    feat: final

--- routes/test.js
+++ routes/test.js
@@ -1,64 +1,69 @@
 import express from "express";
-import { createTable, openDB } from "../utils/dbHandler.js";
+import { createTable, openDB, withDB } from "../utils/dbHandler.js";
 
 const router = express.Router();
-
 router.get("/", async (req, res) => {
-
-  res.send(openDB())
+  res.send("ok");
 });
-router.get("/c", async (req, res) => {
-createTable(`CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT UNIQUE,
-  password TEXT,
-  created_at TEXT DEFAULT CURRENT_TIMESTAMP
-);`)
-  res.send(openDB())
+router.post("/", async (req, res) => {
+  // res.send(openDB())
+  try {
+    let users = [];
+    await withDB(async (db) => {
+      users = await db.all("SELECT * FROM users");
+    });
+    res.send(users); // 返回用户列表
+  } catch (err) {
+    console.error("获取用户失败:", err.message);
+    res.status(500).send("获取用户失败");
+  }
 });
+// router.get("/c", async (req, res) => {
+// createTable(`CREATE TABLE IF NOT EXISTS users (
+//   id INTEGER PRIMARY KEY AUTOINCREMENT,
+//   username TEXT UNIQUE,
+//   password TEXT,
+//   created_at TEXT DEFAULT CURRENT_TIMESTAMP
+// );`)
+//   res.send(openDB())
+// });
+// router.get("/c1", async (req, res) => {
+// const db = await openDB()
+//   await db.run('INSERT INTO users (username, password) VALUES (?, ?)', ['123', '123'])
+//   await db.close()
+//   res.send(openDB())
+// });
+// router.get("/s", async (req, res) => {
+// const db = await openDB()
+//   const users = await db.all('SELECT * FROM users')
 
-
-
-router.get("/c1", async (req, res) => {
-const db = await openDB()
-  await db.run('INSERT INTO users (username, password) VALUES (?, ?)', ['123', '123'])
-  await db.close()
-  res.send(openDB())
-});
-
-router.get("/s", async (req, res) => {
-const db = await openDB()
-  const users = await db.all('SELECT * FROM users')
-
-  res.send(users)
+//   res.send(users)
+// });
+// router.get("/f", async (req, res) => {
+//   createTable(`CREATE TABLE IF NOT EXISTS favorites (
+//     id INTEGER PRIMARY KEY AUTOINCREMENT,
+//     user_id INTEGER NOT NULL,
+//     slug TEXT NOT NULL,
+//     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+//     FOREIGN KEY (user_id) REFERENCES users(id)
+//     );`)
+//     res.send(openDB())
+//   });
+router.post("/initdb", async (req, res) => {
+  createTable(`CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT UNIQUE,
+      password TEXT,
+      created_at TEXT DEFAULT CURRENT_TIMESTAMP
+      );`);
+  createTable(`CREATE TABLE IF NOT EXISTS favorites (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        slug TEXT NOT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users(id)
+        );`);
+  // res.send(openDB())
+  res.send("ok");
 });
 export default router;
-
-router.get("/f", async (req, res) => {
-createTable(`CREATE TABLE IF NOT EXISTS favorites (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_id INTEGER NOT NULL,
-  slug TEXT NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES users(id)
-);`)
-  res.send(openDB())
-});
-
-router.get("/initdb", async (req, res) => {
-createTable(`CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT UNIQUE,
-  password TEXT,
-  created_at TEXT DEFAULT CURRENT_TIMESTAMP
-);`)
-createTable(`CREATE TABLE IF NOT EXISTS favorites (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_id INTEGER NOT NULL,
-  slug TEXT NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES users(id)
-);`)
-  // res.send(openDB())
-  res.send('ok')
-});
